7471ff2a2f4be6ea128400ed7b48a29add37ca8a
郑嘉威 <1831562069@qq.com>
2025-03-28 21:38:36 +0800

    Update test.java

--- test.java
+++ test.java
@@ -6,25 +6,12 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public Result login(User param, @RequestParam("captcha") String captcha, HttpServletRequest request, HttpSession session){
-        // 验证码验证
-        if (!CaptchaUtil.ver(captcha, request)) {
-            return Result.fail("验证码错误！");
+    public Result<Map<String,Object>> login(@RequestBody User user){
+        Map<String,Object> data = userService.login(user);
+        if(data != null){
+            return Result.success(data);
         }
-
-        User user = userService.login(param);
-        if(user != null){
-            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            // 参数1是请求密码，参数2是数据库中加密的值
-            boolean matches = passwordEncoder.matches(param.getPassword(), user.getPassword());
-            if(matches) {
-                // 登录成功
-                user.setPassword(null);
-                session.setAttribute("userInfo", user);
-                return Result.success();
-            }
-        }
-        // 登录失败
-        return Result.fail("用户名或密码错误！");
+        return Result.fail(20002,"用户名或密码错误");
     }
+
 }
